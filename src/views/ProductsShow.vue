6c609ac2a4223c82d3110277f1fb27a16824a8e1
Dan Cross <dancross96@gmail.com>
2022-08-30 13:04:52 -0500

    added index and show action

---
+++ src/views/ProductsShow.vue
@@ -0,0 +1,29 @@
+<script>
+import axios from 'axios';
+export default {
+  data: function () {
+    return {
+      product: {},
+    };
+  },
+  created: function () {
+    console.log('in show product')
+    console.log(this.$route.params.id)
+    axios.get("/products/" + this.$route.params.id + ".json").then(response => {
+      console.log(response.data);
+      this.product = response.data;
+    })
+  },
+  methods: {},
+};
+</script>
+  
+  <template>
+  <div class="products-show">
+    <h1>{{  product.name  }}</h1>
+    <p> {{  product.price  }}</p>
+    <p> {{  product.description  }} </p>
+    <button>Add to Cart</button>
+    <!-- <a v-bind:href="`/products/${products.id}/edit`">Edit</a> -->
+  </div>
+</template>
---
+++ src/views/ProductsView.vue
@@ -0,0 +1,35 @@
+<script>
+import axios from 'axios'
+export default {
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+      products: []
+    };
+  },
+  created: function () {
+    this.indexProducts()
+  },
+  methods: {
+    indexProducts: function () {
+      axios.get("/products").then((response) => {
+        console.log("products index", response);
+        this.products = response.data;
+      });
+    }
+  },
+};
+</script>
+  
+  <template>
+  <div class="home">
+    <div v-for="product in products">
+      <a v-bind:href="`/products/${product.id}`"> {{  product.name  }} Go somewhere</a>
+      <p> {{  product.price  }} </p>
+      <br />
+    </div>
+  </div>
+</template>
+  
+  <style>
+  </style>
